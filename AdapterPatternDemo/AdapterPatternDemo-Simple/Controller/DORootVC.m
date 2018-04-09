b4e24024e75ceb8312340e7675079147c76669a4
Dino <kill_death@163.com>
2018-04-09 18:52:00 +0800

    相关文件配置及创建

---
+++ AdapterPatternDemo/AdapterPatternDemo-Simple/Controller/DORootVC.m
@@ -0,0 +1,25 @@
+//
+//  DORootVC.m
+//  AdapterPatternDemo-Simple
+//
+//  Created by 魏欣宇 on 2018/4/9.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+#pragma mark - Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"简单适配器模式";
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+@end
